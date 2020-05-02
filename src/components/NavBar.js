dd20e51f11014f4df7e871f63c158a9c8cf4dd3b
Djimovanberlo <djimovanberlo@gmail.com>
2020-05-02 12:13:35 +0200

    pages and links setup

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <NavLink exact to="/" activeStyle={{ fontWeight: "bold" }}>
+        Home
+      </NavLink>{" "}
+      <NavLink exact to="/discover" activeStyle={{ fontWeight: "bold" }}>
+        Discover
+      </NavLink>{" "}
+      <NavLink exact to="/about" activeStyle={{ fontWeight: "bold" }}>
+        About
+      </NavLink>
+    </div>
+  );
+}
